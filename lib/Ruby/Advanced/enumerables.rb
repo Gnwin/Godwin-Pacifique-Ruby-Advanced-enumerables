d34e8c782af8b3320cea6cb9523073b7328c1f73
Gnwin <nwgwin@gmail.com>
2021-01-07 14:14:50 +0100

    bundle install

---
+++ lib/Ruby/Advanced/enumerables.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "enumerables/version"
+
+module Ruby
+  module Advanced
+    module Enumerables
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
