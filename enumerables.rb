8d709657348e725841766615a5fc938e83a47818
Gnwin <nwgwin@gmail.com>
2021-01-07 21:51:28 +0100

    correct exercise 1 and 2

--- enumerables.rb
+++ enumerables.rb
@@ -1,37 +1,37 @@
 class Array
   # def my_each
-  # 	iterate = self.length
-  # 	if block_given?
-  # 		idx = 0
-  # 		loop do
-  # 			yield(self[idx])
-  # 			idx += 1
-  # 			break if idx == iterate;
-  # 		end
-  # 	else
-  # 		p self
-  # 	end
-  # end
-
-  def my_each
-    max_length = length
-    if block_given?
-      max_length.times do |i|
-        yield(self[i])
-      end
-    else
-      p self
-    end
+  #   max_length = self.length
+  #   if block_given?
+  #     max_length.times do |i|
+  #       yield(self[i])
+  #     end
+  #   else
+  #     p self
+  #   end
+	# end
+	
+	def my_each
+  	iterate = self.length
+  	if block_given?
+  		idx = 0
+  		loop do
+  			yield(self[idx])
+  			idx += 1
+  			break if idx == iterate;
+  		end
+  	else
+  		p self
+  	end
   end
 
   def my_each_with_index
-    iterate = length
+    iterate = self.length
     if block_given?
       idx = 0
       loop do
         yield(self[idx], idx)
         idx += 1
-        break if idx == iterate
+        break if idx == iterate;
       end
     else
       p self
