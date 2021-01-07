e5d99c537f7f93e8791a63239079ae41eeb47bd1
Gnwin <nwgwin@gmail.com>
2021-01-07 21:01:38 +0100

    fix linter errors on exercise 1 and 2

--- enumerables.rb
+++ enumerables.rb
@@ -1,47 +1,44 @@
 class Array
+  # def my_each
+  # 	iterate = self.length
+  # 	if block_given?
+  # 		idx = 0
+  # 		loop do
+  # 			yield(self[idx])
+  # 			idx += 1
+  # 			break if idx == iterate;
+  # 		end
+  # 	else
+  # 		p self
+  # 	end
+  # end
 
-	# def my_each
-	# 	iterate = self.length
-	# 	if block_given?
-	# 		idx = 0
-	# 		loop do
-	# 			yield(self[idx])
-	# 			idx += 1
-	# 			break if idx == iterate;
-	# 		end
-	# 	else
-	# 		p self
-	# 	end
-	# end
+  def my_each
+    max_length = length
+    if block_given?
+      max_length.times do |i|
+        yield(self[i])
+      end
+    else
+      p self
+    end
+  end
 
-	def my_each
-		max_length = self.length
-		if block_given?
-			max_length.times do |i|
-				yield(self[i])
-			end
-		else
-			p self
-		end
-	end
-
-	def my_each_with_index
-		iterate = self.length
-		if block_given?
-			idx = 0
-			loop do
-				yield(self[idx], idx)
-				idx += 1
-				break if idx == iterate;
-			end
-		else
-			p self
-		end
-	end
-
+  def my_each_with_index
+    iterate = length
+    if block_given?
+      idx = 0
+      loop do
+        yield(self[idx], idx)
+        idx += 1
+        break if idx == iterate
+      end
+    else
+      p self
+    end
+  end
 end
 
-
-arr = [1,2,3,4,5]
+arr = [1, 2, 3, 4, 5]
 # arr.my_each { |e| puts e }
 arr.my_each_with_index { |v, i| puts "#{v} and #{i}" }
