2e4ea2652a7ba0994f68b2d171a0f1fb2301435b
Pacifique Linjanja <pacilinja1@icloud.com>
2021-01-11 17:22:06 +0100

    Code refactored

--- enumerables.rb
+++ enumerables.rb
@@ -1,200 +1,365 @@
+# rubocop:disable all
 module Enumerable
-  # def my_each
-  #   max_length = self.length
-  #   if block_given?
-  #     max_length.times do |i|
-  #       yield(self[i])
-  #     end
-  #   else
-  #     p self
-  #   end
-	# end
-	
-	# 1.
-	def my_each
-  	iterate = self.length
-  	if block_given?
-  		idx = 0
-  		loop do
-  			yield(self[idx])
-  			idx += 1
-  			break if idx == iterate;
-  		end
-  	else
-  		self
-  	end
-  end
-
-	# 2.
-  def my_each_with_index
-    iterate = self.length
-    if block_given?
-      idx = 0
-      loop do
-        yield(self[idx], idx)
-        idx += 1
-        break if idx == iterate;
+    def my_each
+      unless block_given?
+        to_enum(__method__)
+      else
+        i = 0
+        while i < self.size
+          yield self[i]
+          i += 1
+        end
+        self
       end
-    else
-      self
     end
-	end
-	
-	# 3.
-	def my_select
-		iterate = self.length
-		if block_given?
-			ary = []
-			idx = 0
-			loop do
-				if yield(self[idx])
-					ary << self[idx]
-				end
-				idx += 1
-				break if idx == iterate;
-			end
-			ary
-		else
-			self
-		end
-	end
-
-	# 4.
-	def my_all?
-		iterate = self.length
-		if block_given?
-			ary = []
-			idx = 0
-			loop do
-				ary << yield(self[idx])
-				idx += 1
-				break if idx == iterate;
-			end
-			all = true
-			iterate.times do |idx|
-				if ary[idx] == false
-					all = false
-					break
-				end
-			end
-			all = all == false ? false : true ;
-			all
-		else
-			true
-		end
-	end
-
-	# 5.
-	def my_any?;
-		iterate = self.length
-		if block_given?;
-			ary = []
-			idx = 0
-			loop do;
-				ary << yield(self[idx])
-				idx += 1
-				break if idx == iterate;
-			end
-			any = false
-			iterate.times do |idx|;
-				if ary[idx] == true
-					any = true
-					break
-				end
-			end
-			any = any == true ? true : false ;
-			any
-		else
-			true
-		end
-	end
-
-	# 6.
-	def my_none?;
-		iterate = self.length
-		if block_given?;
-			ary = []
-			idx = 0
-			loop do;
-				ary << yield(self[idx])
-				idx += 1
-				break if idx == iterate;
-			end
-			none = true
-			iterate.times do |idx|;
-				if ary[idx] == true
-					none = false
-					break
-				end
-			end
-			none = none == true ? true : false ;
-			none
-		else
-			true
-		end
-	end
-
-	# 7.
-	def my_count;
-		iterate = self.length
-		if block_given?;
-			ary = []
-			idx = 0
-			loop do;
-				ary << yield(self[idx])
-				idx += 1
-				break if idx == iterate;
-			end
-			count = 0
-			iterate.times do |idx|;
-				if ary[idx] == true
-					count += 1
-				end
-			end
-			count
-		else
-			count = self.length
-			count
-		end
-	end
-
-	# 8.
-	def my_map
-		ary = []  
-		self.my_each do |elem|
-			ary << yield(elem)
-		end	
-		ary
-	end
-
-	# 9.
-	def my_inject
-		iterate = self.length
-		if block_given?;
-			sum = 0 
-			idx = 0
-			loop do;
-				sum = yield(sum, self[idx])
-				idx += 1
-				break if idx == iterate;
-			end
-			sum
-		else
-			sum
-		end
-	end
-end
-
-
-arr = [1, 2, 3, 4, 5]
-# arr.my_each { |e| e }
-# arr.my_each_with_index { |v, i| puts "#{v} and #{i}" }
-# arr.my_select { |e| e != 2 }
-# arr.my_all? { |e| e == 2 }
-# %w[ant bear cat].my_all? { |word| word.length >= 3 }
-# %w[ant bear cat].my_any? { |word| word.length >= 4 }
-# %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-# arr.my_count{ |x| x%2==0 } #=> 3
-# arr.my_count
-# arr.my_map { |e| e + 2 }
-# arr.my_inject { |sum, n| sum + n }
+    
+    def my_each_with_index
+      unless block_given?
+        to_enum(__method__)
+      else
+        i = 0
+        while i < self.size
+          yield(self[i], i)
+          i += 1
+        end
+        self
+      end
+    end
+    
+    def my_select
+      unless block_given?
+        to_enum(__method__)
+      else
+        i = 0
+        arr = []
+        while i < self.size
+          if yield self[i]
+            arr << self[i]
+          end
+          i += 1
+        end
+        arr
+      end
+    end
+    
+    def my_all?(word = nil)
+      unless block_given?
+        i = 0
+        j = 0
+          
+        while i < self.size
+          self[i]
+          i += 1
+        end
+        if self.size == 0
+          true
+        elsif word.class == Regexp
+          for element in self
+            if element =~ word
+              j += 1
+            end
+          end
+          j == self.length ? true : false
+    
+        elsif word == Numeric
+    
+          for element in self
+            if element.class <= word
+              j += 1
+            end
+          end
+          j == self.length ? true : false
+        elsif word != nil
+          i = 0
+          while i < self.size
+            if self[i - 1] == self[i]
+              j += 1
+            end
+            i += 1
+          end
+          k = 0
+          for element in self
+            if element.class == word
+              k += 1
+            end
+          end
+    
+          for element in self
+            if element == word
+              k += 1
+            end
+          end
+            
+          k == self.length ? true : false
+          
+        else
+          for element in self do
+            if element == false or element == nil
+              j += 1
+            end
+          end
+          j == 0 ? true : false
+        end
+      else
+        i = 0
+        j = 0
+        while i < self.size
+          yield self[i]
+          i += 1
+        end
+    
+        for element in self
+    
+          if yield(element) == false or yield(element) == nil
+            j += 1
+          end
+        end
+        j == 0 ? true : false
+    
+      end
+    end
+  
+    def my_any?(word = nil)
+      unless block_given?
+        i = 0
+        j = 0
+        while i < self.size
+          self[i]
+          i += 1
+        end
+        if self.size == 0
+          false
+        elsif word.class == Regexp
+    
+          for element in self
+            if element =~ word
+              j += 1
+            end
+          end
+          j > 0 and j <= self.length ? true : false
+    
+        elsif word != nil
+          i = 0
+          k = 0
+          while i < self.size
+            if word == self[i]
+              k += 1
+            end
+            i += 1
+          end
+  
+          for element in self
+            if element.class == word
+              k += 1
+            end
+          end
+          k > 0 ? true : false
+    
+        elsif word == Numeric
+          for element in self
+            if element.class <= word
+              j += 1
+            end
+          end
+            
+          j > 0 and j <= self.length ? true : false
+    
+        else
+          k = 0
+          for element in self do
+            if element == false or element == nil
+              k += 1
+            end
+          end
+          k != self.size ? true : false
+    
+        end
+    
+      else
+        i = 0
+        j = 0
+        while i < self.size
+          yield self[i]
+          i += 1
+        end
+    
+        for element in self
+          if yield element
+            j += 1
+          end
+        end
+        j > 0 and j <= self.size ? true : false
+      end
+    end
+    
+    def my_none?(word = nil)
+      unless block_given?
+        i = 0
+        j = 0
+      while i < self.size
+        self[i]
+        i += 1
+      end
+      if self.size == 0
+        true
+      elsif word.class == Regexp
+    
+        self.my_each do |element|
+          if element =~ word
+            j += 1
+          end
+        end
+           
+        j == 0 ? true : false
+    
+      elsif word != nil
+        if word == Numeric
+          for element in self
+            if element.class <= word
+              j += 1
+            end
+          end
+              
+          j > 0 ? false : true
+        else
+          i = 0
+          k = 0
+          for element in self
+            if element == word
+              k += 1
+            end
+          end
+          while i < self.size
+            if self[i].class == word
+              k += 1
+            end
+            i += 1
+          end
+          k == 0 ? true : false
+              
+        end
+    
+        else
+          j = 0
+          self.my_each do |e|
+            if e == true
+              j += 1
+            end
+          end
+          j == 0 ? true : false
+        end
+        else
+          i = 0
+          while i < self.size
+            yield self[i]
+            i += 1
+          end
+          j = 0
+          self.my_each do |element|
+            if yield element
+              j += 1
+            end
+          end
+          j == 0 ? true : false
+    
+        end
+      end
+    
+      def my_count(val = nil)
+        unless block_given?
+    
+          if val.class != NilClass
+            i = 0
+    
+            for el in self do
+              if el == val
+                i += 1
+              end
+            end
+            i
+          else
+            self.my_each do |count|
+              count += 1
+            end
+            count
+          end
+        else
+          j = 0
+          self.my_each do |element|
+            if yield element
+              j += 1
+            end
+          end
+          j
+        end
+      end
+    
+      def my_map(proc_argument = nil)
+        unless block_given?
+          if proc_argument != nil
+            arr = []
+            for element in self do
+              current = proc_argument.call(element)
+              arr << current
+            end
+            arr
+          else
+            to_enum(__method__)
+          end
+          else
+            arr = []
+            for element in self do
+              current = yield element
+              arr << current
+            end
+            arr
+          end
+        end
+    
+      def my_inject(*param)
+        
+        arr = []
+        i = 0
+        for element in self do
+          arr << element
+        end
+    
+        unless block_given?
+    
+          if param.length == 1
+            acc = param[0]
+            result = arr[0]
+            arr[1..-1].my_each do |el|
+              result = result.send(acc, el)
+            end
+          end
+          if param.length == 2
+            result = param[0]
+            acc = param[1]
+            arr.my_each do |el|
+              result = result.send(acc, el)
+            end
+          end
+        else
+          acc = param[0]
+          result = yield(arr[0], arr[1])
+    
+          arr.my_each do |e|
+            if e.class == String
+              result = yield(result, e)
+            end
+    
+            if i > 1
+              result = yield(result, e)
+            end
+            i += 1
+          end
+          if acc.class <= Numeric
+             result = result * acc
+           end
+    
+      end
+        result
+      end
+    
+    end
